7a857116ba42513d7b277351f5a71292ec3422b2
evgenibazarov296@gmail.com <evgenibazarov296@gmail.com>
2022-10-31 13:53:53 -0400

    New classes added

--- src/test/java/com/cydeo/runner/CukesRunner.java
+++ src/test/java/com/cydeo/runner/CukesRunner.java
@@ -5,17 +5,18 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 
-    @RunWith(Cucumber.class)
-    @CucumberOptions(
-            plugin = {
-                    "html:target/cucumber-report.html",
-                    "rerun:target/rerun.txt"
-            },
-            features = "src/test/resources/features" ,
-            glue = "com/cydeo/step_definitions" ,
-            dryRun = false ,
-            tags = "@femaleScientist"
-    )
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt"
+        },
+        features = "src/test/resources/features" ,
+        glue = "com/cydeo/step_definitions" ,
+        dryRun = false,
+        tags = "@femaleScientist"
+)
+
     public class CukesRunner {
 
 }
